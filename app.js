61cadf01dcbdffa6a404c9b1f186fe4b7d0e7bfe
Anirudh Kumar <anirudhkumar790@gmail.com>
2019-05-22 17:42:02 +0530

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://root:pass@123@cluster0-anxtv.mongodb.net/local_library?retryWrites=true';
+// var mongoDB = 'mongodb+srv://root:pass@123@cluster0-anxtv.mongodb.net/local_library?retryWrites=true';
+
+var dev_db_url = 'mongodb+srv://root:pass@123@cluster0-anxtv.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
